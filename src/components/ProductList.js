3ad5a7612259e3f5f2968955722fff3dce44395a
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-03-04 09:45:27 +0700

    display category

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,25 +3,38 @@ import axios from "axios";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/products")
-      .then(response => {
-        const sortedProducts = response.data.sort((a, b) => a.quantity - b.quantity);
-        setProducts(sortedProducts);
-      });
+    axios.get("http://localhost:5000/products").then((response) => {
+      const sortedProducts = response.data.sort(
+        (a, b) => a.quantity - b.quantity
+      );
+      setProducts(sortedProducts);
+    });
+    //Get category
+    axios.get("http://localhost:5000/categories").then((response) => {
+      setCategories(response.data);
+    });
   }, []);
+
+  //Find category name form category id
+  const getCategoryName = (categoryId) => {
+    const category = categories.find(c => Number(c.id) === Number(categoryId));
+    return category ? category.name : "Undefined";
+  };
 
   return (
     <div>
       <h2>List Clothes</h2>
-      <table>
+      <table border={1}>
         <thead>
           <tr>
             <th>Id</th>
             <th>Name</th>
             <th>Date of import</th>
             <th>Quantity</th>
+            <th>Category</th>
           </tr>
         </thead>
         <tbody>
@@ -31,6 +44,7 @@ function ProductList() {
               <td>{p.name}</td>
               <td>{p.date}</td>
               <td>{p.quantity}</td>
+              <td>{getCategoryName(p.categoryId)}</td>
             </tr>
           ))}
         </tbody>
